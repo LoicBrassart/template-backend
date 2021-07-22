34d089d40f8f35f9b2c601abe5a516d54af6d731
Hacene010 <hamrounehacene06@gmail.com>
2021-07-22 16:15:10 +0200

    set up passport for react admin authentification

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const passport = require('passport');
+
 const textsRouter = require('./routes/texts.js');
 const imagesRouter = require('./routes/images.js');
 const { port, frontendUrl, adminUrl } = require('./config.js');
 const linksRouter = require('./routes/links.js');
+const authRouter = require('./routes/auth.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(passport.initialize());
 app.use(cors({ credentials: true, origin: { frontendUrl, adminUrl } }));
 
 app.use('/texts', textsRouter);
 app.use('/images', imagesRouter);
 app.use('/links', linksRouter);
+app.use('/auth', authRouter);
 
 app.use('/', (req, res) => {
   res.status(404).send('Route not found! ');
---
+++ routes/auth.js
@@ -0,0 +1,65 @@
+const authRouter = require('express').Router();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { connection, saltRounds, secretKey } = require('../config');
+require('../strategies');
+
+authRouter.get('/signup', async (req, res) => {
+  try {
+    const [rows] = await connection.query(
+      'SELECT id, lastname, firstname, email FROM user'
+    );
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+authRouter.post('/signup', async (req, res) => {
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    parseInt(saltRounds, 10)
+  );
+  const sql = 'INSERT INTO user SET ?';
+  const sqlValues = req.body;
+  try {
+    const [results] = await connection.query(sql, sqlValues);
+    const id = results.insertId;
+    req.body.id = id;
+    delete req.body.password;
+    const token = jwt.sign(req.body, secretKey);
+    res.json({ user: req.body, token });
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') {
+      res.status(409).send('This user already exists !');
+    }
+    res.status(500).send(err);
+  }
+});
+
+authRouter.delete('/signup/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'DELETE FROM user WHERE id=?';
+  const sqlCount = 'SELECT COUNT(*) as data FROM user';
+  const sqlValues = [id];
+  try {
+    const [[resCount]] = await connection.query(sqlCount);
+    if (resCount.data === 1) {
+      res.status(409).send('Error deleting an user');
+    } else {
+      const [results] = await connection.query(sql, sqlValues);
+
+      res.status(204).json(results);
+    }
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+authRouter.post('/login', passport.authenticate('local'), (req, res) => {
+  const token = jwt.sign(JSON.stringify(req.user), secretKey);
+  res.json({ user: req.user, token });
+});
+
+module.exports = authRouter;
